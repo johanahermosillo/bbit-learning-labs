ac4475eada904615aa586deb011f20ac332eb778
Kuike <kuike@Kuikes-MacBook-Air.local>
2026-02-10 17:55:08 -0700

    finished producer_sol

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/solution/producer_sol.py
@@ -2,7 +2,7 @@ import pika
 from producer_interface import mqProducerInterface
 
 class mqProducer(mqProducerInterface):
-    def __init__(self, routing_key: str, exchange_name: str) --> None:
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
 
         #save instance variables
         self.routing_key = routing_key
@@ -17,4 +17,30 @@ class mqProducer(mqProducerInterface):
 
     def setupRMQConnection(self) -> None:
 
-        parameters = pika.ConnectionParameters('localhost')
+        parameters = pika.ConnectionParameters(host='rabbitmq')
+
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+
+        #Direct exchange type
+        self.channel.exchange_declare(
+            exchange=self.exchange_name,
+            exchange_type='direct',
+            durable=True
+        )
+
+    def publishOrder(self, message: str) -> None:
+        if not self.channel or self.channel.is_closed:
+            #reconnect in case connection was closed
+            self.setupRMQConnection()
+
+        self.channel.basic_publish(
+            exchange=self.exchange_name,
+            routing_key=self.routing_key,
+            body=message,
+        )
+
+        if self.channel and self.channel.is_open:
+            self.channel.close()
+        if self.connection and self.connection.is_open:
+            self.connection.close()
